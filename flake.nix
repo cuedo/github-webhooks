a868e8f33312b567b7db3b256d15e47868c74273
Julian K. Arni <jkarni@gmail.com>
2025-07-16 16:54:05 +0200

    Add flake file

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = nixpkgs.legacyPackages.${system};
+        haskellPackages = pkgs.haskell.packages.ghc98;
+    in {
+      packages.default = haskellPackages.callCabal2nix "github-webhooks" ./. {};
+      devShells.default = haskellPackages.shellFor {
+        packages = _: [self.packages.${system}.default];
+        buildInputs = [ pkgs.cabal-install ];
+      };
+  });
+}
