f2bb31418f6c46bde120a4dc8af94a48351984f0
Julian K. Arni <jkarni@gmail.com>
2025-07-22 15:05:09 +0200

    Update description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake";
+  description = "A flake for developing or using github-webhooks";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
